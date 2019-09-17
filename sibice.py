27af006b2013a914e44f8c811cc3bf72b13228b8
AJO <alex.andunge@gmail.com>
2019-09-17 18:05:55 +0200

    Fikset sibice, prøvde apaxians

--- sibice.py
+++ sibice.py
@@ -1,10 +1,11 @@
 import math
 numbers = input().split()
 
-pyt = math.sqrt(int(numbers[1]/2)**2+int(numbers[2])**2)
+pyt = math.sqrt((int(numbers[1]))**2+int(numbers[2])**2)
 
-for i in range(4,len(numbers)+1):
-    if int(numbers[i]) <= int(numbers[1]) or int(numbers[i]) <= int(numbers[2]) or int(numbers[i]) <= pyt:
+for i in range(int(numbers[0])):
+    inn = int(input())
+    if (inn <= int(numbers[1])) or (inn <= int(numbers[2])) or (inn <= pyt):
         print("DA")
     else:
         print("NE")
