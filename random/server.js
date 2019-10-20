ef0188fb8d25b51ddf7fa78fc3ede71691ff6a34
AJO <alex.andunge@gmail.com>
2019-10-20 22:23:34 +0200

    Added parts of debuglog to server.js

--- random/server.js
+++ random/server.js
@@ -1,17 +1,19 @@
 // Dependencies
 const http = require('http');
+const util = require('util');
+const debugLog = util.debuglog('server');
 
 //create a server:
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  console.log('Write response')
+  debugLog('Write response');
   res.write('Hello NodeJS! This is a new local server <br/>') //write a response to the client
   res.write('Your url: ' + req.url);
   res.end() //end the response
-  console.log('Response ended.')
+  debugLog('Response ended.');
 });
 
 // start listening to the server
 server.listen(8080, () => {
-  console.log('Node server is listening on 8080 port.')
+  debugLog('Node server is listening on 8080 port.')
 });
