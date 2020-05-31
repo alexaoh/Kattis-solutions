85c783fe4db75eb8833fdde504673066fdaa98a8
alexaoh <alex.andunge@gmail.com>
2020-05-31 18:08:53 +0200

    oddManOut.js gives correct output in Terminal, but wrong in Kattis

--- nodejs/oddManOut.js
+++ nodejs/oddManOut.js
@@ -1,35 +1,43 @@
 const readline = require('readline');
 const readlineInterface = readline.createInterface(process.stdin, process.stdout);
- 
-function ask(){
-    let res = 0;
-    readlineInterface.question("", (guests) => {
-        readlineInterface.question("", (line) => {
-            let l = line.split(" ");
-            res = l[0];
-            for (let i = 1; i < l.length; i++){
-                res = res ^ l[i]; //XOR
-            }
-            //readlineInterface.close();
-        });
-        //readlineInterface.close();
-        return res;
+
+function input(){
+    return new Promise((resolve, reject) => {
+        readlineInterface.question("", resolve);
     });
 }
-
-
-//Må bruke async/await promises? Skjønner ikke helt akkurat nå, test mer. 
-
-readlineInterface.question("", (testCases) => {
-    let l = [];
-    for (let i = 0; i < parseInt(testCases, 10); i++){
-        l.push(ask());
+ 
+async function oddManOut(){
+    let testCases = await input(); 
+    let results = [];
+    for (let i = 0; i < testCases; i++){
+        let guests = await input();
+        let intString = await input();
+        const intList = intString.split(" ");
+        let res = intList[0];
+        let copyIntList = [...intList]; //Make copy of listInt.
+        let j = 1;
+        while(copyIntList.length > 1){
+            if (res === copyIntList[j]){
+                copyIntList.splice(j, 1);
+                copyIntList.splice(0, 1);
+                res = copyIntList[0];
+                j = 0;
+            }
+            if (j === copyIntList.length){
+                break;
+            }
+            j++;
+        }
+        results.push(res);
     }
 
     let i = 1;
-    for (const val of l){
+    for (const val of results){
         console.log("Case #"+i+": "+val);
         i++;
-    }
-    //readlineInterface.close();
-});
+    }    
+    process.exit();
+}
+
+oddManOut();
